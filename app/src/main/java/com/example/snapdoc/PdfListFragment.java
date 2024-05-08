d1bbd25fabf86821407b3912ae3d6ffb3d858340
Ana-Maria Suditu <anamaria_suditu@yahoo.com>
2024-05-08 21:23:59 +0300

    finished pdf list

--- app/src/main/java/com/example/snapdoc/PdfListFragment.java
+++ app/src/main/java/com/example/snapdoc/PdfListFragment.java
@@ -1,19 +1,33 @@
 package com.example.snapdoc;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.snapdoc.adapters.AdapterPdf;
+import com.example.snapdoc.models.ModelPdf;
+
+import java.io.File;
+import java.util.ArrayList;
+
 public class PdfListFragment extends Fragment {
 
+    private RecyclerView pdfRv;
     private Context mContext;
+
+    private ArrayList<ModelPdf> pdfArrayList;
+    private AdapterPdf adapterPdf;
+    private static final String TAG = "PDF_LIST_TAG";
 
     public PdfListFragment() {
         // Required empty public constructor
@@ -33,5 +47,32 @@ public class PdfListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
+
+        pdfRv = view.findViewById(R.id.pdfRv);
+        loadPdfDocuments();
+    }
+
+    private void loadPdfDocuments(){
+
+        pdfArrayList = new ArrayList<>();
+        adapterPdf = new AdapterPdf(mContext, pdfArrayList);
+        pdfRv.setAdapter(adapterPdf);
+        File folder = new File(mContext.getExternalFilesDir(null),Constants.PDF_FOLDER);
+
+        if(folder.exists()){
+            File[] files = folder.listFiles();
+            Log.d(TAG,"loadPdfDocuments: Files Count: " + files.length);
+
+            for(File fileEntry: files){
+                Log.d(TAG, "loadPdfDocuments: File Name: " + fileEntry.getName());
+
+                Uri uri = Uri.fromFile(fileEntry);
+
+                ModelPdf modelPdf = new ModelPdf(fileEntry, uri);
+                pdfArrayList.add(modelPdf);
+
+                adapterPdf.notifyItemInserted(pdfArrayList.size());
+            }
+        }
     }
 }
