8de7dded29a46c93d3b99e275d62fcead4c1411d
Ana-Maria Suditu <anamaria_suditu@yahoo.com>
2024-05-21 22:06:47 +0300

    solved glide error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,18 +46,18 @@ dependencies {
     implementation(libs.material)
     implementation(libs.constraintlayout)
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(libs.appcompat)
+    implementation(libs.material.v1120)
+    implementation(libs.constraintlayout)
 
     // Glide library for image processing/loading
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler.v500rc01)
 
     // Testing libraries
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 
     // Other existing dependencies
     implementation(libs.recyclerview)
