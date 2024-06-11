2be166825261ddb47be24a1aac59e433bb56ca3e
alexandramocanu1 <alexandramocanu74@gmail.com>
2024-06-11 20:14:58 +0300

    .

--- app/src/main/java/com/example/snapdoc/ui/TextViewFragment.java
+++ app/src/main/java/com/example/snapdoc/ui/TextViewFragment.java
@@ -1,27 +1,44 @@
 package com.example.snapdoc.ui;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.snapdoc.R;
 import com.google.android.material.button.MaterialButton;
-import com.google.mlkit.vision.common.InputImage;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.google.mlkit.vision.text.Text;
-import com.google.mlkit.vision.text.TextRecognition;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
 
 public class TextViewFragment extends Fragment {
 
@@ -29,6 +46,12 @@ public class TextViewFragment extends Fragment {
 
     private ImageView imageView;
     private EditText recognisedText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,6 +71,112 @@ public class TextViewFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_text, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.text_item_delete) {
+            showDeleteConfirmationDialog();
+            return true;
+        } else if (itemId == R.id.text_item_pdf) {
+            showPdfOptionDialog();
+
+
+        }
+        return false;
+    }
+
+//
+//
+//    @Override
+//    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+//        MenuInflater inflater = mode.getMenuInflater();
+//        inflater.inflate(R.menu.menu_text, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+//        // You can update menu items here if needed
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+//        int itemId = item.getItemId();
+//        if (itemId == R.id.text_item_delete) {
+//            // Handle delete action
+//            mode.finish(); // Finish the action mode after performing the action
+//            return true;
+//        } else if (itemId == R.id.text_item_pdf) {
+//            // Handle PDF conversion action
+//            mode.finish();
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    @Override
+//    public void onDestroyActionMode(ActionMode mode) {
+//        // Cleanup actions if needed
+//    }
+//
+//
+//
+//    private void startActionMode() {
+//        FragmentActivity activity = getActivity();
+//        if (activity instanceof AppCompatActivity) {
+//            AppCompatActivity appCompatActivity = (AppCompatActivity) activity;
+//            appCompatActivity.startSupportActionMode((androidx.appcompat.view.ActionMode.Callback) this);
+//        }
+//    }
+    private void showPdfOptionDialog() {
+        new AlertDialog.Builder(getContext())
+                .setMessage("Convert to an existing PDF or create a new one?")
+                .setPositiveButton("New PDF", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        createNewPdf();
+                    }
+                })
+                .setNegativeButton("Existing PDF", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        addToExistingPdf();
+                    }
+                })
+                .show();
+    }
+
+    private void showDeleteConfirmationDialog() {
+        new AlertDialog.Builder(getContext())
+                .setMessage("Do you want to delete all?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        recognisedText.setText("");
+                        Toast.makeText(getContext(), "Text deleted", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    private void createNewPdf() {
+        // Logic for creating a new PDF
+        Toast.makeText(getContext(), "New PDF created", Toast.LENGTH_SHORT).show();
+    }
+
+    private void addToExistingPdf() {
+        // Logic for adding to an existing PDF
+        Toast.makeText(getContext(), "Added to existing PDF", Toast.LENGTH_SHORT).show();
     }
 
     private void dispatchTakePictureIntent() {
@@ -77,6 +206,7 @@ public class TextViewFragment extends Fragment {
                     @Override
                     public void onSuccess(Text visionText) {
                         displayRecognizedText(visionText);
+                        //startActionMode();
                     }
                 })
                 .addOnFailureListener(new com.google.android.gms.tasks.OnFailureListener() {
@@ -94,4 +224,100 @@ public class TextViewFragment extends Fragment {
         }
         recognisedText.setText(resultText.toString());
     }
+    //package com.example.snapdoc.ui;
+//
+//import android.app.AlertDialog;
+//import android.content.DialogInterface;
+//import android.os.Bundle;
+//import android.view.LayoutInflater;
+//import android.view.Menu;
+//import android.view.MenuInflater;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.EditText;
+//import android.widget.Toast;
+//
+//import androidx.annotation.NonNull;
+//import androidx.fragment.app.Fragment;
+//
+//import com.example.snapdoc.R;
+//import com.google.android.material.imageview.ShapeableImageView;
+//
+//public class TextViewFragment extends Fragment {
+//
+//    private EditText recognisedTextBtn;
+//    private ShapeableImageView imageIq;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setHasOptionsMenu(true);
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_text_list, container, false);
+//
+//        imageIq = view.findViewById(R.id.imageIq);
+//        recognisedTextBtn = view.findViewById(R.id.recognisedTextBtn);
+//
+//        return view;
+//    }
+//
+//    @Override
+//    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+//        inflater.inflate(R.menu.menu_text, menu);
+//        super.onCreateOptionsMenu(menu, inflater);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        if(item.getItemId())
+//        switch (item.getItemId()) {
+//            case R.id.text_item_delete:
+//                showDeleteConfirmationDialog();
+//                return true;
+//            case R.id.text_item_pdf:
+//                showPdfOptionDialog();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+//
+//    private void showDeleteConfirmationDialog() {
+//        new AlertDialog.Builder(getContext())
+//                .setMessage("Do you want to delete all?")
+//                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        recognisedTextBtn.setText("");
+//                        Toast.makeText(getContext(), "Text deleted", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .setNegativeButton("No", null)
+//                .show();
+//    }
+//
+//    private void showPdfOptionDialog() {
+//        new AlertDialog.Builder(getContext())
+//                .setMessage("Convert to an existing PDF or create a new one?")
+//                .setPositiveButton("New PDF", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        createNewPdf();
+//                    }
+//                })
+//                .setNegativeButton("Existing PDF", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        addToExistingPdf();
+//                    }
+//                })
+//                .show();
+//    }
+//
+
 }
